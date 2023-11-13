90c48121fc2c6f47944021c3a634d766dba3fd88
yee ✧ <60899552+pleavinn@users.noreply.github.com>
2023-11-13 11:29:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+
+function mostrarSlide(n) {
+    const slides = document.querySelectorAll('.slide');
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+function mudarSlide(n) {
+    mostrarSlide(slideIndex += n);
+}
+
+// Inicializa o carrossel
+mostrarSlide(slideIndex);
